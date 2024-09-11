723c14e0ba0ea9610775b38d90ecdf96d9a41af2
LegendaryGeek <timothydaley4@gmail.com>
2024-09-11 19:27:21 -0400

    test import commented to see if dependencies are working

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -27,11 +27,15 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+//This Import fails due to classpath dependencies being missing
+//import io.github.edwinmindcraft.apoli.api.power.factory;
+
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
 public class ExampleMod
 {
+    //PowerFactory
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
     // Directly reference a slf4j logger
