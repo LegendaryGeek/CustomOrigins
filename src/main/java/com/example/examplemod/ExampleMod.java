a7491c7668ae8d38a5a9296e9f8ff7decc6b75e2
Robin Seifert <rseifert.phone@gmail.com>
2024-09-11 20:01:25 -0400

    updating origins dependencies to 1.19.2

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -27,8 +27,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-//This Import fails due to classpath dependencies being missing
-//import io.github.edwinmindcraft.apoli.api.power.factory;
+import io.github.edwinmindcraft.apoli.api.power.factory;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
